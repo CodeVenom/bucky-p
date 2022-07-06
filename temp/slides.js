32e6c516642193fa27edbf60b56d2937ee937ad8
CodeVenom <juri.jaeger@gmail.com>
2022-07-06 22:37:11 +0200

    second slide

--- temp/slides.js
+++ temp/slides.js
@@ -1,8 +1,15 @@
 // - - - - - slides handling - - - - -
+const DEFAULT_STATUS_OPTIONS = {
+    invert: false,
+};
+
 const RESET_STACK = [
     slide(0, invis),
     slide(0, flatten),
     slideParts(0, invis_offset),
+    slide(0, invertStatus(invis)),
+    slide(1, invis),
+    slidePartsNested(1, shrink, 3),
 ];
 
 let presentationStack = [];
@@ -31,8 +38,12 @@ function randomizedStatus(e, s) {
     }
 }
 
-function pushStatusToBackStack(e, ...s) {
-    let flag = true;
+function pushStatusToBackStack(
+    e,
+    o = DEFAULT_STATUS_OPTIONS,
+    ...s
+) {
+    let flag = !o.invert;
     const f = _ => {
         log(s);
         if (flag) {
@@ -46,16 +57,53 @@ function pushStatusToBackStack(e, ...s) {
     presentationBackStack.unshift(f);
 }
 
-function flatten(e) {
-    pushStatusToBackStack(e, 'flat');
+function flatten(e, o) {
+    pushStatusToBackStack(
+        e,
+        o,
+        'flat',
+    );
 }
 
-function invis(e) {
-    pushStatusToBackStack(e, 'invis');
+function invis(e, o) {
+    pushStatusToBackStack(
+        e,
+        o,
+        'invis',
+    );
 }
 
-function invis_offset(e) {
-    pushStatusToBackStack(e, 'invis', 'offset');
+function shrink(e, o) {
+    pushStatusToBackStack(
+        e,
+        o,
+        'shrink',
+    );
+}
+
+function invis_offset(e, o) {
+    pushStatusToBackStack(
+        e,
+        o,
+        'invis',
+        'offset',
+    );
+}
+
+// TODO: define this as a function prototype
+function invertStatus(f) {
+    return withOptions(
+        f,
+        {
+            invert: true,
+        },
+    );
+}
+
+function withOptions(f, o) {
+    return e => {
+        f(e, o);
+    };
 }
 
 function slide(index, f) {
@@ -70,6 +118,31 @@ function slideParts(index, f) {
             f(e);
         }
     };
+}
+
+function slidePartsNested(index, f, depth) {
+    return _ => {
+        const elements = nestedChildren(
+            [document.getElementsByClassName('slide')[index]],
+            depth,
+        );
+        for (const e of elements) {
+            f(e);
+        }
+    };
+}
+
+function nestedChildren(elements, depth) {
+    if (depth === 0) {
+        return elements;
+    }
+    const stack = [];
+    for (const e of elements) {
+        for (const c of e.children) {
+            stack.push(c);
+        }
+    }
+    return nestedChildren(stack, depth - 1);
 }
 
 function reset() {
